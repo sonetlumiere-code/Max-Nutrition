bab3e922b3616c81fbcb6ad6f4ed905beeafd014
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-06-03 00:01:40 -0300

    installed shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
-  return <>Home home</>
+  return (
+    <>
+      Home
+      <Button>Hello</Button>
+    </>
+  )
 }
