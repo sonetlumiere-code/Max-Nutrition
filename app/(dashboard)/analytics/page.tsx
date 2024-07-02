7ac70a94f67eaf3798d23ff4dd0759928415f0ac
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-07-01 23:49:52 -0300

    cache session

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -1,8 +1,13 @@
-export default function AnalyiticsPage() {
+import getSession from "@/lib/auth/get-session"
+
+export default async function AnalyiticsPage() {
+  const session = await getSession()
+
   return (
     <>
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Proximamente..</h1>
+      <div className='flex items-center'>
+        <h1 className='text-lg font-semibold md:text-2xl'>Proximamente..</h1>
+        {JSON.stringify(session, null, 4)}
       </div>
     </>
   )
