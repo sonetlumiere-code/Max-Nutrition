04ac99df095196485af90afe46536ba44f7839a4
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-07-23 23:40:28 -0300

    edit product

---
+++ app/(dashboard)/products/edit-product/[productId]/page.tsx
@@ -0,0 +1,55 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { redirect } from "next/navigation"
+import { getProduct } from "@/data/products"
+import EditProduct from "@/components/dashboard/products/edit-product/edit-product"
+
+interface EditProductPageProps {
+  params: {
+    productId: string
+  }
+}
+
+const EditProductPage = async ({ params }: EditProductPageProps) => {
+  const { productId } = params
+
+  const product = await getProduct({
+    where: { id: productId },
+  })
+
+  if (!product) {
+    redirect("/welcome")
+  }
+
+  return (
+    <div className='space-y-6'>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Inicio</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/products'>Productos</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Editar Producto</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <h2 className='font-semibold text-lg'>Editar Receta</h2>
+
+      <EditProduct product={product} />
+    </div>
+  )
+}
+
+export default EditProductPage
---
+++ components/dashboard/products/edit-product/edit-product.tsx
@@ -0,0 +1,255 @@
+"use client"
+
+import { Product } from "@prisma/client"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { useForm } from "react-hook-form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { useRouter } from "next/navigation"
+import { toast } from "@/components/ui/use-toast"
+import { productSchema } from "@/lib/validations/product-validation"
+import { editProduct } from "@/actions/products/edit-product"
+import { Switch } from "@/components/ui/switch"
+import uploadImage from "@/actions/cloudinary/upload-image"
+import deleteImage from "@/actions/cloudinary/delete-image"
+
+type ProductSchema = z.infer<typeof productSchema>
+
+type EditProductProps = {
+  product: Product
+}
+
+const EditProduct = ({ product }: EditProductProps) => {
+  const router = useRouter()
+
+  const form = useForm<ProductSchema>({
+    resolver: zodResolver(productSchema),
+    defaultValues: product,
+  })
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting, isValid },
+  } = form
+
+  const onSubmit = async (data: ProductSchema) => {
+    if (data.imageFile?.length) {
+      const formData = new FormData()
+      formData.append("imageFile", data.imageFile[0])
+      const uploadRes = await uploadImage(formData)
+      data.image = uploadRes.public_id || ""
+      data.imageFile = []
+
+      if (product.image && uploadRes.public_id) {
+        deleteImage(product.image.split("/")[1])
+      }
+    }
+
+    const res = await editProduct({ id: product.id, values: data })
+
+    if (res.success) {
+      router.push("/products")
+      toast({
+        title: "Producto actualizada",
+        description: "El producto se actualizó correctamente.",
+      })
+    }
+
+    if (res.error) {
+      toast({
+        variant: "destructive",
+        title: "Error actualizando producto.",
+        description: res.error,
+      })
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={handleSubmit(onSubmit)} className='grid gap-6'>
+        <Card className='max-w-screen-md'>
+          <CardHeader></CardHeader>
+          <CardContent>
+            <div className='space-y-3'>
+              <FormField
+                control={control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Editar producto'
+                        disabled={isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name='description'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Descripción</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Describa el producto'
+                        disabled={isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name='price'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Precio (AR$)</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='number'
+                        step='0.1'
+                        placeholder='Precio en pesos'
+                        disabled={isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={control}
+                name='promotionalPrice'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Precio promocional (AR$)</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='number'
+                        step='0.1'
+                        placeholder='Precio promocional en pesos'
+                        disabled={isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='imageFile'
+                render={({ field: { value, onChange, ...fieldProps } }) => (
+                  <FormItem>
+                    <FormLabel>Imagen</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...fieldProps}
+                        type='file'
+                        onChange={(event) => onChange(event.target.files)}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='featured'
+                render={({ field }) => (
+                  <FormItem className='flex flex-row items-center justify-between rounded-lg border p-4'>
+                    <div className='space-y-0.5'>
+                      <FormLabel className='text-base'>Destacado</FormLabel>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        disabled={isSubmitting}
+                        aria-readonly
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='stock'
+                render={({ field }) => (
+                  <FormItem className='flex flex-row items-center justify-between rounded-lg border p-4'>
+                    <div className='space-y-0.5'>
+                      <FormLabel className='text-base'>Stock</FormLabel>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        disabled={isSubmitting}
+                        aria-readonly
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name='show'
+                render={({ field }) => (
+                  <FormItem className='flex flex-row items-center justify-between rounded-lg border p-4'>
+                    <div className='space-y-0.5'>
+                      <FormLabel className='text-base'>Mostrar</FormLabel>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        disabled={isSubmitting}
+                        aria-readonly
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type='submit' disabled={isSubmitting || !isValid}>
+              {isSubmitting && (
+                <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+              )}
+              Editar Producto
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  )
+}
+
+export default EditProduct
