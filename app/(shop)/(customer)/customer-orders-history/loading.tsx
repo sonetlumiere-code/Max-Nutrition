52495871ad1338baa6649f6a9ed2c6d1c35c357b
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-10-12 19:08:36 -0300

    fix h-screen

--- app/(shop)/(customer)/customer-orders-history/loading.tsx
+++ app/(shop)/(customer)/customer-orders-history/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const Loading = () => {
   return (
-    <div className='space-y-6'>
+    <div className='h-screen space-y-6'>
       <Skeleton className='w-36 h-10' />
       <Skeleton className='h-1/2' />
     </div>
