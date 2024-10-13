8dfcc1a982e1835dfc01b5d7e8463cfbe078eeda
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-10-12 22:03:49 -0300

    fix orders page skeleton

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -82,18 +82,6 @@ export default function OrdersPage() {
     setSelectedTab(tab)
   }
 
-  if (isLoading) {
-    return (
-      <div className='h-screen space-y-4'>
-        <div className='flex justify-between'>
-          <Skeleton className='h-10 w-36' />
-          <Skeleton className='h-10 w-36' />
-        </div>
-        <Skeleton className='h-1/2' />
-      </div>
-    )
-  }
-
   if (error) {
     return <div>Error buscando pedidos.</div>
   }
@@ -101,91 +89,105 @@ export default function OrdersPage() {
   return (
     <main className='grid flex-1 items-start gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
       <div className='grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2'>
-        <Tabs
-          defaultValue={selectedTab}
-          onValueChange={(value) => handleTabChange(value as TimePeriod)}
-        >
-          <div className='flex items-center'>
-            <TabsList>
-              <TabsTrigger value='week'>Semana</TabsTrigger>
-              <TabsTrigger value='month'>Mes</TabsTrigger>
-              <TabsTrigger value='year'>Año</TabsTrigger>
-              <TabsTrigger value='all'>Todos</TabsTrigger>
-            </TabsList>
-            <div className='ml-auto flex items-center gap-2'>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant='outline'
-                    size='sm'
-                    className='h-7 gap-1 text-sm'
-                  >
-                    <ListFilter className='h-3.5 w-3.5' />
-                    <span className='sr-only sm:not-sr-only'>Filter</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align='end'>
-                  <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuCheckboxItem
-                    checked={filters.Pending}
-                    onCheckedChange={() => toggleFilter("Pending")}
-                  >
-                    Pendiente
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem
-                    checked={filters.Accepted}
-                    onCheckedChange={() => toggleFilter("Accepted")}
-                  >
-                    Aceptado
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem
-                    checked={filters.Completed}
-                    onCheckedChange={() => toggleFilter("Completed")}
-                  >
-                    Completado
-                  </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem
-                    checked={filters.Cancelled}
-                    onCheckedChange={() => toggleFilter("Cancelled")}
-                  >
-                    Cancelado
-                  </DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <Button size='sm' variant='outline' className='h-7 gap-1 text-sm'>
-                <File className='h-3.5 w-3.5' />
-                <span className='sr-only sm:not-sr-only'>Export</span>
-              </Button>
+        {isLoading ? (
+          <div className='h-screen space-y-4'>
+            <div className='flex justify-between'>
+              <Skeleton className='h-10 w-36' />
+              <Skeleton className='h-10 w-36' />
             </div>
+            <Skeleton className='h-1/2' />
           </div>
-          <TabsContent value={selectedTab}>
-            <Card>
-              <CardHeader className='px-7'>
-                <CardTitle>Pedidos</CardTitle>
-                <CardDescription>
-                  Pedidos recientes de tu tienda.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                {filteredOrders && filteredOrders.length > 0 ? (
-                  <OrdersList
-                    orders={filteredOrders}
-                    setSelectedOrder={setSelectedOrder}
-                  />
-                ) : (
-                  <div className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm h-64 p-6'>
-                    <div className='flex flex-col items-center gap-1 text-center'>
-                      <h3 className='text-2xl font-bold tracking-tight'>
-                        Todavía no tenés ningún pedido
-                      </h3>
+        ) : (
+          <Tabs
+            defaultValue={selectedTab}
+            onValueChange={(value) => handleTabChange(value as TimePeriod)}
+          >
+            <div className='flex items-center'>
+              <TabsList>
+                <TabsTrigger value='week'>Semana</TabsTrigger>
+                <TabsTrigger value='month'>Mes</TabsTrigger>
+                <TabsTrigger value='year'>Año</TabsTrigger>
+                <TabsTrigger value='all'>Todos</TabsTrigger>
+              </TabsList>
+              <div className='ml-auto flex items-center gap-2'>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant='outline'
+                      size='sm'
+                      className='h-7 gap-1 text-sm'
+                    >
+                      <ListFilter className='h-3.5 w-3.5' />
+                      <span className='sr-only sm:not-sr-only'>Filter</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align='end'>
+                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem
+                      checked={filters.Pending}
+                      onCheckedChange={() => toggleFilter("Pending")}
+                    >
+                      Pendiente
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                      checked={filters.Accepted}
+                      onCheckedChange={() => toggleFilter("Accepted")}
+                    >
+                      Aceptado
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                      checked={filters.Completed}
+                      onCheckedChange={() => toggleFilter("Completed")}
+                    >
+                      Completado
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem
+                      checked={filters.Cancelled}
+                      onCheckedChange={() => toggleFilter("Cancelled")}
+                    >
+                      Cancelado
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Button
+                  size='sm'
+                  variant='outline'
+                  className='h-7 gap-1 text-sm'
+                >
+                  <File className='h-3.5 w-3.5' />
+                  <span className='sr-only sm:not-sr-only'>Export</span>
+                </Button>
+              </div>
+            </div>
+            <TabsContent value={selectedTab}>
+              <Card>
+                <CardHeader className='px-7'>
+                  <CardTitle>Pedidos</CardTitle>
+                  <CardDescription>
+                    Pedidos recientes de tu tienda.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  {filteredOrders && filteredOrders.length > 0 ? (
+                    <OrdersList
+                      orders={filteredOrders}
+                      setSelectedOrder={setSelectedOrder}
+                    />
+                  ) : (
+                    <div className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm h-64 p-6'>
+                      <div className='flex flex-col items-center gap-1 text-center'>
+                        <h3 className='text-2xl font-bold tracking-tight'>
+                          Todavía no tenés ningún pedido
+                        </h3>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </CardContent>
-            </Card>
-          </TabsContent>
-        </Tabs>
+                  )}
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        )}
       </div>
       <div>
         <OrderItemDetails selectedOrder={selectedOrder} />
