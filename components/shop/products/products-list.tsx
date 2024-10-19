2c15bd3ed3ffd87ed7436cadc02081812d9224c0
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-10-18 21:34:09 -0300

    fix products list

--- components/shop/products/products-list.tsx
+++ components/shop/products/products-list.tsx
@@ -8,15 +8,19 @@ const ProductsList = async () => {
     <div className='grid gap-8'>
       {categories?.map((category) => (
         <div className='grid gap-4' key={category.id}>
-          <h2 className='text-xl font-semibold'>{category.name}</h2>
-          <div className='grid gap-6'>
-            {category.products?.map(
-              (product) =>
-                product.show && (
-                  <ProductItem key={product.id} product={product} />
-                )
-            )}
-          </div>
+          {category.products.length > 0 && (
+            <>
+              <h2 className='text-xl font-semibold'>{category.name}</h2>
+              <div className='grid gap-6'>
+                {category.products?.map(
+                  (product) =>
+                    product.show && (
+                      <ProductItem key={product.id} product={product} />
+                    )
+                )}
+              </div>
+            </>
+          )}
         </div>
       ))}
     </div>
