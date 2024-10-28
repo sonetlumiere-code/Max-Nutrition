d97538f5f74658f3a4910321cbde7b4df6405aa9
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-10-28 19:28:38 -0300

    removed search product

--- components/shop/layout/header/header-shop.tsx
+++ components/shop/layout/header/header-shop.tsx
@@ -46,14 +46,14 @@ export default async function HeaderShop() {
             className='object-cover'
           />
         </Link>
-        <div className='hidden lg:inline relative flex-1 max-w-md mx-4'>
+        {/* <div className='hidden lg:inline relative flex-1 max-w-md mx-4'>
           <Search className='absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-muted-foreground' />
           <Input
             type='search'
             placeholder='Buscar producto..'
             className='w-full rounded-full bg-muted pl-10 pr-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary'
           />
-        </div>
+        </div> */}
 
         {session?.user ? (
           <div className='flex items-center space-x-4'>
