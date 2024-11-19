9c0fb6a87100f71ec963b6a7134852405576146d
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-11-18 22:07:19 -0300

    use sync cart

--- components/shop/cart/cart.tsx
+++ components/shop/cart/cart.tsx
@@ -24,43 +24,12 @@ import { MoveLeftIcon } from "lucide-react"
 import { useMediaQuery } from "@/hooks/use-media-query"
 import CartContent from "./cart-content"
 import Link from "next/link"
-import { useGetCategories } from "@/hooks/use-get-categories"
-import { toast } from "@/components/ui/use-toast"
+import useSyncCart from "@/hooks/use-sync-cart"
 
 const Cart = () => {
-  const { items, setItems, open, setOpen } = useCart()
+  const { items, open, setOpen } = useCart()
 
-  useGetCategories({
-    onSuccess: (categories) => {
-      if (!categories) return
-
-      const allProducts = categories.flatMap((category) => category.products)
-
-      const updatedItems = items.map((item) => {
-        const updatedProduct = allProducts.find(
-          (product) => product?.id === item.product.id
-        )
-        if (
-          updatedProduct &&
-          new Date(updatedProduct.updatedAt) > new Date(item.product.updatedAt)
-        ) {
-          return { ...item, product: updatedProduct }
-        }
-        return item
-      })
-
-      const filteredItems = updatedItems.filter((item) =>
-        allProducts.some((product) => product?.id === item.product.id)
-      )
-
-      if (JSON.stringify(filteredItems) !== JSON.stringify(items)) {
-        setItems(filteredItems)
-        toast({
-          title: "Algunos productos del carrito fueron actualizados.",
-        })
-      }
-    },
-  })
+  useSyncCart()
 
   const isDesktop = useMediaQuery("(min-width: 768px)")
 
---
+++ hooks/use-sync-cart.ts
@@ -0,0 +1,43 @@
+"use client"
+
+import { useCart } from "@/components/cart-provider"
+import { toast } from "@/components/ui/use-toast"
+import { useGetCategories } from "./use-get-categories"
+
+const useSyncCart = () => {
+  const { items, setItems } = useCart()
+
+  useGetCategories({
+    onSuccess: (categories) => {
+      if (!categories) return
+
+      const allProducts = categories.flatMap((category) => category.products)
+
+      const updatedItems = items.map((item) => {
+        const updatedProduct = allProducts.find(
+          (product) => product?.id === item.product.id
+        )
+        if (
+          updatedProduct &&
+          new Date(updatedProduct.updatedAt) > new Date(item.product.updatedAt)
+        ) {
+          return { ...item, product: updatedProduct }
+        }
+        return item
+      })
+
+      const filteredItems = updatedItems.filter((item) =>
+        allProducts.some((product) => product?.id === item.product.id)
+      )
+
+      if (JSON.stringify(filteredItems) !== JSON.stringify(items)) {
+        setItems(filteredItems)
+        toast({
+          title: "Algunos productos del carrito fueron actualizados.",
+        })
+      }
+    },
+  })
+}
+
+export default useSyncCart
