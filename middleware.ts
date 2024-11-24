72c4c9bbca771e82c6849c3a110dbf45bed23058
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-11-24 00:55:44 -0300

    test middleware env

--- middleware.ts
+++ middleware.ts
@@ -13,15 +13,22 @@ const { auth } = NextAuth(authConfig)
 
 const secret = process.env.AUTH_SECRET
 
+const baseURL = process.env.BASE_URL
+
 export default auth(async (req) => {
   const { nextUrl } = req
   const isLoggedIn = !!req.auth
+
+  const BASEURL = process.env.BASE_URL
+  console.log(BASEURL)
 
   const token = await getToken({ req, secret })
   const userRole = (token?.role as Role) || "USER"
 
   console.log(token)
   console.log(userRole)
+
+  console.log(baseURL)
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
