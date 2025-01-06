1bd7e5acc2aace786ff127121a53d5f1b55d52c8
sonetlumiere-code <lucas.perez.ok@gmail.com>
2025-01-05 23:04:10 -0300

    customers data table

--- app/(dashboard)/customers/page.tsx
+++ app/(dashboard)/customers/page.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-import DeleteCustomer from "@/components/dashboard/customers/delete-customer/delete-customer"
+import CustomersDataTable from "@/components/dashboard/customers/list/customers-data-table/customers-data-table"
 import { Icons } from "@/components/icons"
 import {
   Breadcrumb,
@@ -14,30 +13,12 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import UserAvatar from "@/components/user-avatar"
 import { getCustomers } from "@/data/customer"
 import { auth } from "@/lib/auth/auth"
 import { cn } from "@/lib/utils"
-import { format } from "date-fns"
 import Link from "next/link"
 import { redirect } from "next/navigation"
 
@@ -57,8 +38,6 @@ export default async function CustomersPage() {
     },
   })
 
-  const customersLength = customers?.length || 0
-
   return (
     <>
       <Breadcrumb>
@@ -77,124 +56,52 @@ export default async function CustomersPage() {
         <h1 className='text-lg font-semibold md:text-2xl'>Clientes</h1>
       </div>
 
-      <Card>
-        <CardHeader>
-          <div className='space-between flex items-center'>
-            <div className='max-w-screen-sm'>
-              <CardTitle className='text-xl'>Clientes</CardTitle>
-              <CardDescription>
-                Administra tus clientes y visualiza su información.
-              </CardDescription>
+      {customers && customers.length > 0 ? (
+        <Card>
+          <CardHeader>
+            <div className='space-between flex items-center'>
+              <div className='max-w-screen-sm'>
+                <CardTitle className='text-xl'>Clientes</CardTitle>
+                <CardDescription>
+                  Administra tus clientes y visualiza su información.
+                </CardDescription>
+              </div>
+              <div className='ml-auto'>
+                <Link
+                  href='customers/create-customer'
+                  className={cn(buttonVariants({ variant: "default" }))}
+                >
+                  <>
+                    <Icons.circlePlus className='mr-2 h-4 w-4' />
+                    Agregar
+                  </>
+                </Link>
+              </div>
             </div>
-            <div className='ml-auto'>
-              <Link
-                href='customers/create-customer'
-                className={cn(buttonVariants({ variant: "default" }))}
-              >
-                <>
-                  <Icons.circlePlus className='mr-2 h-4 w-4' />
-                  Agregar
-                </>
+          </CardHeader>
+          <CardContent>
+            <CustomersDataTable customers={customers} />
+          </CardContent>
+        </Card>
+      ) : (
+        <div className='flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm'>
+          <div className='flex flex-col items-center gap-1 text-center'>
+            <h3 className='text-2xl font-bold tracking-tight'>
+              Todavía no tenés ningún cliente
+            </h3>
+            <p className='text-sm text-muted-foreground'>
+              Cargá tu primer cliente haciendo click en el siguiente botón
+            </p>
+
+            <Button className='mt-4' asChild>
+              <Link href='/customers/create-customer'>
+                <Icons.circlePlus className='mr-2 h-4 w-4' />
+                Agregar cliente
               </Link>
-            </div>
+            </Button>
           </div>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className='hidden w-[100px] sm:table-cell'>
-                  <span className='sr-only'>Imagen</span>
-                </TableHead>
-                <TableHead>Nombre</TableHead>
-                <TableHead className='hidden sm:table-cell'>Email</TableHead>
-                <TableHead className='hidden md:table-cell'>Teléfono</TableHead>
-                <TableHead className='hidden md:table-cell'>
-                  Creado el
-                </TableHead>
-                <TableHead className='text-end'>
-                  <span>Acciones</span>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {customers?.map((customer) => (
-                <TableRow key={customer.id}>
-                  <TableCell className='hidden sm:table-cell'>
-                    {customer.user ? (
-                      <UserAvatar user={customer.user} />
-                    ) : (
-                      <Icons.circleUser className='h-11 w-11 text-muted-foreground' />
-                    )}
-                  </TableCell>
-                  <TableCell className='font-medium'>{customer.name}</TableCell>
-                  <TableCell className='hidden sm:table-cell'>
-                    {customer.user ? (
-                      <Button
-                        variant='link'
-                        size='default'
-                        className='group px-0'
-                      >
-                        {customer.user?.email}
-                        <Icons.clipboard className='h-4 w-4 ml-1 invisible group-hover:visible' />
-                      </Button>
-                    ) : null}
-                  </TableCell>
-                  <TableCell className='hidden md:table-cell'>
-                    {customer.phone ? (
-                      <Button variant='outline' size='default'>
-                        {customer.phone}
-                        <Icons.messageSquareMore className='h-4 w-4 ml-1' />
-                      </Button>
-                    ) : (
-                      "-"
-                    )}
-                  </TableCell>
-                  <TableCell className='hidden md:table-cell'>
-                    {format(customer.createdAt, "dd/MM/yyyy")}
-                  </TableCell>
-                  <TableCell className='text-end'>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          aria-haspopup='true'
-                          size='icon'
-                          variant='ghost'
-                        >
-                          <Icons.moreHorizontal className='h-4 w-4' />
-                          <span className='sr-only'>Mostrar menú</span>
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align='end'>
-                        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                        <Link href={`customers/${customer.id}`}>
-                          <DropdownMenuItem>
-                            <Icons.eye className='h-4 w-4 mr-2' /> Ver
-                          </DropdownMenuItem>
-                        </Link>
-                        <Link href={`customers/edit-customer/${customer.id}`}>
-                          <DropdownMenuItem>
-                            <Icons.pencil className='h-4 w-4 mr-2' /> Editar
-                          </DropdownMenuItem>
-                        </Link>
-                        <DropdownMenuItem>
-                          <DeleteCustomer customer={customer} />
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-        <CardFooter>
-          <div className='text-xs text-muted-foreground'>
-            Mostrando <strong>{customersLength}</strong> client
-            {customersLength > 1 ? "es" : "e"}
-          </div>
-        </CardFooter>
-      </Card>
+        </div>
+      )}
     </>
   )
 }
---
+++ components/dashboard/customers/list/customers-data-table/customers-data-table.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table"
+import { useEffect, useState } from "react"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { columns } from "./columns"
+import { PopulatedCustomer } from "@/types/types"
+import { Input } from "@/components/ui/input"
+import { DataTablePagination } from "@/components/ui/data-table-pagination"
+import { DataTableViewOptions } from "@/components/ui/data-table-view-options"
+import { useMediaQuery } from "@/hooks/use-media-query"
+
+interface OrdersDataTableProps {
+  customers: PopulatedCustomer[]
+}
+
+const CustomersDataTable = ({ customers }: OrdersDataTableProps) => {
+  const [sorting, setSorting] = useState<SortingState>([])
+  const [filtering, setFiltering] = useState<string>("")
+
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  const table = useReactTable({
+    data: customers,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    state: {
+      sorting,
+      globalFilter: filtering,
+    },
+    onGlobalFilterChange: setFiltering,
+  })
+
+  useEffect(() => {
+    table.setColumnVisibility({
+      shippingMethod: isDesktop,
+      createdAt: isDesktop,
+    })
+  }, [isDesktop, table])
+
+  return (
+    <div className='grid gap-4'>
+      <div className='flex items-center gap-2'>
+        <Input
+          placeholder='Filtrar'
+          onChange={(e) => setFiltering(e.target.value)}
+          className='max-w-xs'
+        />
+        <DataTableViewOptions table={table} />
+      </div>
+      <div className='rounded-md border'>
+        <Table className='relative'>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'
+                >
+                  No hay resultados.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-between px-2 h-9'>
+        <div className='flex items-center'>{/* Bulk Actions */}</div>
+        <DataTablePagination table={table} setPageSize />
+      </div>
+    </div>
+  )
+}
+
+export default CustomersDataTable
