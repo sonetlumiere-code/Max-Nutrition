f06ecbe0ce9fca37bfc806a3620160fd1de00cc9
sonetlumiere-code <lucas.perez.ok@gmail.com>
2025-01-11 12:57:45 -0300

    shop shipping zones

--- components/shop/buttons-info-shop.tsx
+++ components/shop/buttons-info-shop.tsx
@@ -2,6 +2,8 @@ import dynamic from "next/dynamic"
 import { getPromotions } from "@/data/promotions"
 import { Icons } from "../icons"
 import { Card, CardContent } from "../ui/card"
+import { getShippingZones } from "@/data/shipping-zones"
+import ShippingZones from "./shipping-zones/shipping-zones"
 
 const Promotions = dynamic(() => import("./promotions/promotions"), {
   ssr: false,
@@ -14,17 +16,32 @@ const ButtonsInfoShop = async () => {
     },
   })
 
+  const shippingZones = await getShippingZones()
+
   return (
     <div className='w-full max-w-3xl mx-auto p-4'>
       <div className='grid grid-cols-2 gap-4'>
-        <Card className='w-full cursor-pointer hover:bg-gray-50 transition-colors'>
-          <CardContent className='flex flex-col items-center justify-center p-2 sm:p-4 h-full'>
-            <Icons.truck className='w-8 h-8 mb-2' />
-            <p className='text-center text-xs sm:text-sm'>
-              Ver zonas y costos de envío
-            </p>
-          </CardContent>
-        </Card>
+        {shippingZones && shippingZones.length > 0 ? (
+          <ShippingZones shippingZones={shippingZones}>
+            <Card className='w-full cursor-pointer hover:bg-gray-50 transition-colors'>
+              <CardContent className='flex flex-col items-center justify-center p-2 sm:p-4 h-full'>
+                <Icons.truck className='w-8 h-8 mb-2' />
+                <p className='text-center text-xs sm:text-sm'>
+                  Ver zonas y costos de envío
+                </p>
+              </CardContent>
+            </Card>
+          </ShippingZones>
+        ) : (
+          <Card className='w-full'>
+            <CardContent className='flex flex-col items-center justify-center p-2 sm:p-4 h-full'>
+              <Icons.truck className='w-8 h-8 mb-2' />
+              <p className='text-center text-xs sm:text-sm'>
+                Sin zonas de envío disponibles
+              </p>
+            </CardContent>
+          </Card>
+        )}
 
         {promotions && promotions.length > 0 ? (
           <Promotions promotions={promotions}>
---
+++ components/shop/shipping-zones/shipping-zones.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useMediaQuery } from "@/hooks/use-media-query"
+import { ReactNode } from "react"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { ShippingZone } from "@prisma/client"
+
+type ShippingZonesProps = {
+  children: ReactNode
+  shippingZones: ShippingZone[]
+}
+
+const ShippingZones = ({ children, shippingZones }: ShippingZonesProps) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  if (isDesktop) {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogContent className='sm:max-w-[600px]'>
+          <DialogHeader>
+            <DialogTitle>Zonas de envío</DialogTitle>
+            <DialogDescription>Zonas de envío disponibles</DialogDescription>
+          </DialogHeader>
+
+          {shippingZones.map((shippingZone) => (
+            <span key={shippingZone.id}>{shippingZone.municipality}</span>
+          ))}
+
+          <DialogFooter className='flex flex-col'>
+            <DialogClose asChild>
+              <Button type='button' variant='outline'>
+                <Icons.moveLeftIcon className='w-4 h-4 mr-3' /> Volver
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
+  }
+
+  return (
+    <div className='relative flex items-center'>
+      <Drawer>
+        <DrawerTrigger asChild>{children}</DrawerTrigger>
+        <DrawerContent className='min-h-[40vh]'>
+          <DrawerHeader>
+            <DrawerTitle>Zonas de envío</DrawerTitle>
+            <DrawerDescription>Zonas de envío disponibles</DrawerDescription>
+          </DrawerHeader>
+
+          <div className='space-y-3 p-4'>
+            {shippingZones.map((shippingZone) => (
+              <span key={shippingZone.id}>{shippingZone.municipality}</span>
+            ))}
+          </div>
+
+          <DrawerFooter className='border-t-2 lg:border-t-0'>
+            <DrawerClose asChild>
+              <Button type='button' variant='outline'>
+                <Icons.moveLeftIcon className='w-4 h-4 mr-3' /> Volver
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  )
+}
+
+export default ShippingZones
