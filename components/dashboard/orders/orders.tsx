74183eaeca07d50d28f0409d32a11588c33274d1
sonetlumiere-code <lucas.perez.ok@gmail.com>
2025-03-24 19:50:53 -0300

    include product recipe type

--- components/dashboard/orders/orders.tsx
+++ components/dashboard/orders/orders.tsx
@@ -52,6 +52,7 @@ const fetchOrders = async () => {
                       },
                     },
                   },
+                  type: true,
                 },
               },
             },
